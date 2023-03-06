84530d592e617a34a23e20e716f4ee37fc923e6b
sr2mg <55787575+sr2mg@users.noreply.github.com>
2023-03-06 21:40:39 +0900

    refactor: #1 MotionCtrlリファクタ
    - motionを場合分けせずにSetするように変更
    - motionListに有効なmotionを規定し、それに紐づいてなければfalseで返すようにした

--- Assets/Scripts/MotionCtrl.cs
+++ Assets/Scripts/MotionCtrl.cs
@@ -9,6 +9,7 @@ public class MotionCtrl : MonoBehaviour
     public int motion;
     public Animator animCtrl;
 
+    private list motionList = [0,1,2,3,4]
     public void Start()
     {
         animCtrl = this.gameObject.GetComponent<Animator>();
@@ -16,30 +17,12 @@ public class MotionCtrl : MonoBehaviour
 
     public void Update()
     {
-        if (motion == 0)
-        {
-            animCtrl.SetInteger("motion", 0);
+        // もしmotionにmotionListの中の値が入っていなかったらFalseで返す。できればExceptionでエラー発生させるか、選択式にすると良いかも
+        var isMotionInList = Array.Exists(motionList, element => element == motion);
+        if (isMotionInList == false){
+            return false;
         }
+        animCtrl.SetInteger("motion", motion);
 
-        if (motion == 1)
-        {
-            animCtrl.SetInteger("motion", 1);
-        }
-
-        if (motion == 2)
-        {
-            animCtrl.SetInteger("motion", 2);
-        }
-
-
-        if (motion == 3)
-        {
-            animCtrl.SetInteger("motion", 3);
-        }
-
-        if (motion == 4)
-        {
-            animCtrl.SetInteger("motion", 4);
-        }
     }
 }
